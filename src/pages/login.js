cb92855780a9287a334867ffbe4345d69407c264
Diana <135276904+Diana-Cl@users.noreply.github.com>
2024-12-17 06:02:43 -0800

    Update login.js

--- src/pages/login.js
+++ src/pages/login.js
@@ -3,14 +3,17 @@ export async function renderLoginPage () {
     <!DOCTYPE html>
     <html lang="en">
     <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>User Login</title>
-    <style>
-        :root {
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="https://kit.fontawesome.com/69b597926c.js" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+        <title>User Login</title>
+        <style>
+          :root {
             --color: black;
             --primary-color: #09639f;
-            --header-color: #09639f; 
+            --header-color: #09639f;
             --background-color: #fff;
             --form-background-color: #f9f9f9;
             --lable-text-color: #333;
@@ -18,90 +21,114 @@ export async function renderLoginPage () {
             --border-color: #ddd;
             --input-background-color: white;
             --header-shadow: 2px 2px 4px rgba(0, 0, 0, 0.25);
-        }
-        html, body { height: 100%; margin: 0; }
-        body {
-            font-family: system-ui;
-            background-color: var(--background-color);
-            position: relative;
-            overflow: hidden;
-        }
-        body.dark-mode {
-            --color: white;
-            --primary-color: #09639F;
-            --header-color: #3498DB; 
-            --background-color: #121212;
-            --form-background-color: #121212;
-            --lable-text-color: #DFDFDF;
-            --h2-color: #D5D5D5;
-            --border-color: #353535;
-            --input-background-color: #252525;
-            --header-shadow: 2px 2px 4px rgba(255, 255, 255, 0.25);
-        }
-        html, body { height: 100%; margin: 0; }
-        .container {
-            position: absolute;
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%, -50%);
-            width: 90%;
-        }
-        h1 { font-size: 2.5rem; text-align: center; color: var(--header-color); margin: 0 auto 30px; text-shadow: var(--header-shadow); }        
-        h2 { text-align: center; color: var(--h2-color) }
-        .form-container {
-            background: var(--form-background-color);
-            border: 1px solid var(--border-color);
-            border-radius: 10px;
-            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-            padding: 20px;
-        }
-        .form-control { margin-bottom: 15px; display: flex; align-items: center; }
-        label {
-            display: block;
-            margin-bottom: 5px;
-            padding-right: 20px;
-            font-size: 110%;
-            font-weight: 600;
-            color: var(--lable-text-color);
-        }
-        input[type="text"],
-        input[type="password"] {
-            width: 100%;
-            padding: 10px;
-            border: 1px solid var(--border-color);
-            border-radius: 5px;
-            color: var(--lable-text-color);
-            background-color: var(--input-background-color);
-        }
-        button {
-            display: block;
-            width: 100%;
-            padding: 10px;
-            font-size: 16px;
-            font-weight: 600;
-            border: none;
-            border-radius: 5px;
-            color: white;
-            background-color: var(--primary-color);
-            cursor: pointer;
-            transition: background-color 0.3s ease;
-        }
-        .button:hover,
-        button:focus {
-            background-color: #2980b9;
-            box-shadow: 0 8px 15px rgba(0, 0, 0, 0.3);
-            transform: translateY(-2px);
-        }
-        button.button:hover { color: white; }
-        .button:active { transform: translateY(1px); box-shadow: 0 3px 7px rgba(0, 0, 0, 0.3); }
-        @media only screen and (min-width: 768px) {
-            .container { width: 30%; }
-        }
+      }
+      html, body { height: 100%; margin: 0; }
+      body {
+          font-family: system-ui;
+          background-color: var(--background-color);
+          position: relative;
+          overflow: hidden;
+      }
+      body.dark-mode {
+          --color: white;
+          --primary-color: #09639F;
+          --header-color: #3498DB;
+          --background-color: #121212;
+          --form-background-color: #121212;
+          --lable-text-color: #DFDFDF;
+          --h2-color: #D5D5D5;
+          --border-color: #353535;
+          --input-background-color: #252525;
+          --header-shadow: 2px 2px 4px rgba(255, 255, 255, 0.25);
+      }
+      html, body { height: 100%; margin: 0; }
+      .container {
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%, -50%);
+          width: 90%;
+      }
+      h1 { font-size: 2.5rem; text-align: center; color: var(--header-color); margin: 0 auto 30px; text-shadow: var(--header-shadow); }        
+      h2 { text-align: center; color: var(--h2-color) }
+      .form-container {
+          background: var(--form-background-color);
+          border: 1px solid var(--border-color);
+          border-radius: 10px;
+          box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+          padding: 20px;
+      }
+      .form-control { margin-bottom: 15px; display: flex; align-items: center; }
+      label {
+          display: block;
+          margin-bottom: 5px;
+          padding-right: 20px;
+          font-size: 110%;
+          font-weight: 600;
+          color: var(--lable-text-color);
+      }
+      input[type="text"],
+      input[type="password"] {
+          width: 100%;
+          padding: 10px;
+          border: 1px solid var(--border-color);
+          border-radius: 5px;
+          color: var(--lable-text-color);
+          background-color: var(--input-background-color);
+      }
+      button {
+          display: block;
+          width: 100%;
+          padding: 10px;
+          font-size: 16px;
+          font-weight: 600;
+          border: none;
+          border-radius: 5px;
+          color: white;
+          background-color: var(--primary-color);
+          cursor: pointer;
+          transition: background-color 0.6s ease;
+      }
+      .button:hover,
+      button:focus {
+          background-color: #2980b9;
+          box-shadow: 0 8px 15px rgba(0, 0, 0, 0.3);
+          transform: translateY(-2px);
+      }
+      button.button:hover { color: white; }
+      .button:active { transform: translateY(2px); box-shadow: 0 3px 7px rgba(0, 0, 0, 0.3); }
+      @media only screen and (min-width: 768px) {
+        .container { width: 30%; }
+      }
+      .button {
+      background: linear-gradient(45deg,
+        #09639f, rgba(32, 195, 190, 0.67), 
+        #3498db 34.14%, #3498db, #00ffd5, #3498db 51.5%);
+        background-size: 200%;
+        color: white;
+        font-weight: 600;
+        animation: rainbow 20s linear infinite;
+        transition: all 0.3s ease;
+      }
+      .button:hover {
+        transform: translateY(-2px);
+        box-shadow: 0 8px 15px rgba(0, 0, 0, 0.2);
+        animation: rainbow 5s linear infinite;
+       }
+        @keyframes rainbow {
+          0% { background-position: 0% 50%; }
+          50% { background-position: 100% 50%; }
+          100% { background-position: 0% 50%; }
+       }
+        @keyframes textclip {
+          0% { background-position: 200% center; }
+          100% { background-position: -200% center; }
+      }
     </style>
     </head>
     <body>
         <div class="container">
-            <h1>BPB Panel <span style="font-size: smaller;">${globalThis.panelVersion}</span> 💦</h1>
+            <h1><i class="fa-sharp fa-solid fa-beat-fade">BPB PANEL 👻 <span style="font-size:14px; position: fixed; right: 0px; margin-top: 43px;">${globalThis.panelVersion}</span></i></h1>
             <div class="form-container">
                 <h2>User Login</h2>
                 <form id="loginForm">
@@ -130,7 +157,7 @@ export async function renderLoginPage () {
                 });
             
                 if (!response.ok) {
-                    passwordError.textContent = '⚠️ Wrong Password!';
+                    passwordError.textContent = '❌ Wrong Password! ❌';
                     const errorMessage = await response.text();
                     console.error('Login failed:', errorMessage);
                     return;
